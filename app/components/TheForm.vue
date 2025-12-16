1113ef779057eab4db7a5d6c45d0f89696360872
Dmytriy Pozniakov <145044135+DmitriyPozniakov@users.noreply.github.com>
2025-12-16 16:10:53 +0200

    added connection form with backend

--- app/components/TheForm.vue
+++ app/components/TheForm.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue"
 import { useI18n } from "vue-i18n"
+const { locale } = useI18n({ useScope: 'global' })
 
 const phoneInput = ref(null)
 const { $phone } = useNuxtApp()
@@ -21,6 +22,43 @@ onMounted(() => {
         })
     }
 })
+
+const currentLang = ref(locale.value);
+
+const firstName = ref(null);
+const lastName = ref(null)
+const email = ref(null)
+const phoneNumber = ref(null);
+
+const submitForm = async () => {
+    console.log(firstName.value, lastName.value, email.value, phoneNumber.value, currentLang.value);
+    try {
+        const response = await fetch('https://api.tek9.gg/rest-api/aipo', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                firstName: firstName.value,
+                lastName: lastName.value,
+                email: email.value,
+                phone: phoneNumber.value,
+                language: currentLang.value
+            })
+        })
+
+        if(!response.ok) {
+            throw new Error('err')
+        }
+
+        const data = await response.json();
+        console.log(data)
+
+    } catch(err) {
+        console.log(err)
+    }
+}
+
 </script>
 
 <template>
@@ -28,27 +66,27 @@ onMounted(() => {
         <div class="cf-container">
             <h2 class="cf-title">{{ t('heroNew.form.title') }}</h2>
 
-            <form class="cf-form">
+            <form class="cf-form" @submit.prevent="submitForm">
                 <div class="cf-group">
-                    <input name="fname" type="text" :placeholder="t('heroNew.form.firstName')" required class="cf-input"
+                    <input name="fname" type="text" v-model="firstName" :placeholder="t('heroNew.form.firstName')" required class="cf-input"
                         :aria-label="t('heroNew.form.firstName')">
                     <div class="cf-error" data-error-for="fname"></div>
                 </div>
 
                 <div class="cf-group">
-                    <input name="lname" type="text" :placeholder="t('heroNew.form.lastName')" required class="cf-input"
+                    <input name="lname" type="text" v-model="lastName" :placeholder="t('heroNew.form.lastName')" required class="cf-input"
                         :aria-label="t('heroNew.form.lastName')">
                     <div class="cf-error" data-error-for="lname"></div>
                 </div>
 
                 <div class="cf-group">
-                    <input name="email" type="email" :placeholder="t('heroNew.form.email')" required class="cf-input"
+                    <input name="email" type="email" v-model="email" :placeholder="t('heroNew.form.email')" required class="cf-input"
                         :aria-label="t('heroNew.form.email')">
                     <div class="cf-error" data-error-for="email"></div>
                 </div>
 
                 <div class="cf-group">
-                    <input ref="phoneInput" type="tel" name="phone" required class="cf-input cf-phone iti__tel-input"
+                    <input ref="phoneInput" type="tel" v-model="phoneNumber" name="phone" required class="cf-input cf-phone iti__tel-input"
                         :aria-label="t('heroNew.form.phone')" dir="ltr" autocomplete="off"
                         placeholder="(0-612) 34567" />
 
